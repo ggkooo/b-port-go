75af6601be4688b27e1c92c0629fb9b21cc170bd
Giordano Bruno Biasi Berwig <giordanoberwig@proton.me>
2026-03-03 21:18:29 -0300

    fix: updated bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,17 +1,21 @@
 <?php
 
+use App\Http\Middleware\EnsureApiKeyIsValid;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
+        api: __DIR__.'/../routes/api.php',
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'api.key' => EnsureApiKeyIsValid::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
